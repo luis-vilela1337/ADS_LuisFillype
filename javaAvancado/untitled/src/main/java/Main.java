42dd60d70152e37c3ad1fc914d442f6c3028fac7
Luis Fillype Vilela Cunha <luis.cunha@contavoltz.com>
2023-08-21 20:08:56 -0300

    feat: create class main and instantiate animals

---
+++ javaAvancado/untitled/src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+    Animal[] animais = new Animal[4];
+
+    animais[0]= new Animal(1L, "Tartaruga");
+    animais[1]= new Animal(2L, "Coelho");
+    animais[2]= new Animal(3L, "Cachorro");
+    animais[3]= new Animal(4L, "Gato");
+
+
+        System.out.println("Arrays");
+        for (int i =0; i< animais.length; i++) {
+            System.out.println(animais[i].getNome());
+        }
+    }
+}
