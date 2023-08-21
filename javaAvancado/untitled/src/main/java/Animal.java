c41d20a539452bc62e01670a222d18f8d476de88
Luis Fillype Vilela Cunha <luis.cunha@contavoltz.com>
2023-08-21 20:08:14 -0300

    feat: create class animal

---
+++ javaAvancado/untitled/src/main/java/Animal.java
@@ -0,0 +1,17 @@
+public class Animal {
+    private Long id;
+    private String nome;
+
+    public Animal(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
