62eda4e5f4ec99634698fd61678c119637266e12
Luis Fillype Vilela Cunha <luis.cunha@contavoltz.com>
2023-08-28 18:41:54 -0300

    refactor: add javadocs

--- exercicosHeranca/heranca/src/main/java/familia/Filha.java
+++ exercicosHeranca/heranca/src/main/java/familia/Filha.java
@@ -1,8 +1,21 @@
 package familia;
 
-import java.sql.Statement;
-
-public class Filha extends Pai{
+/**
+ * Esta classe representa uma filha na família, estendendo a classe Pai.
+ * Contém informações específicas sobre a filha, como nome e cor dos olhos,
+ * além de herdar algumas informações básicas do pai.
+ *
+ * @author Luis Fillype VIlela Cunha
+ * @version 1.0
+ */
+public class Filha extends Pai {
+    /**
+     * O nome da filha.
+     */
     static String nome = "Luciana";
+
+    /**
+     * A cor dos olhos da filha.
+     */
     static String olhos = "Castanhos";
 }
